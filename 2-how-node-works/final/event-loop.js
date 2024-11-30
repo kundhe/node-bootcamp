1bd0239524cdd74ce4d8c181441399ef97448b9d
Okundalaye Great <okundalayegreat@gmail.com>
2024-11-30 14:46:56 +0100

    fourth commit

--- 2-how-node-works/final/event-loop.js
+++ 2-how-node-works/final/event-loop.js
@@ -1,15 +1,12 @@
 const fs = require("fs");
 const crypto = require("crypto");
 
-const start = Date.now();
-process.env.UV_THREADPOOL_SIZE = 4;
-
 setTimeout(() => console.log("Timer 1 finished"), 0);
 setImmediate(() => console.log("Immediate 1 finished"));
 
-fs.readFile("test-file.txt", () => {
+fs.readFile("test-file.xt", () => {
   console.log("I/O finished");
-  console.log("----------------");
+  console.log("---------------------");
 
   setTimeout(() => console.log("Timer 2 finished"), 0);
   setTimeout(() => console.log("Timer 3 finished"), 3000);
@@ -17,17 +14,9 @@ fs.readFile("test-file.txt", () => {
 
   process.nextTick(() => console.log("Process.nextTick"));
 
-  crypto.pbkdf2Sync("password", "salt", 100000, 1024, "sha512");
-  console.log(Date.now() - start, "Password encrypted");
-
-  crypto.pbkdf2Sync("password", "salt", 100000, 1024, "sha512");
-  console.log(Date.now() - start, "Password encrypted");
-
-  crypto.pbkdf2Sync("password", "salt", 100000, 1024, "sha512");
-  console.log(Date.now() - start, "Password encrypted");
-
-  crypto.pbkdf2Sync("password", "salt", 100000, 1024, "sha512");
-  console.log(Date.now() - start, "Password encrypted");
+  crypto.pbkdf2("password", "salt", 100000, 1024, "sha512", () => {
+    console.log(Date.now() - start, "password encrypted");
+  });
 });
 
-console.log("Hello from the top-level code");
+console.log("Hello from the top level code");
